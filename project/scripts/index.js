8353bc511673b1e95d6a6b717a8868e9f48d1e14
EzeG92 <gim24001@byui.edu>
2024-10-21 22:06:02 -0300

    js changes

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -1,3 +1,5 @@
+import { apiFetch } from "./api.js";
+
 //add event listener to menu button and nav links
 const hamButton = document.querySelector('#menuButton');
 const navigation = document.querySelector('.navigation');
@@ -109,39 +111,6 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-//  Get the data from the API
-const url = 'https://api.fxratesapi.com/latest?api_key=fxr_live_6304ed229a999613501abd46016d94c7b452';
-
-//  Get JSON data from API
-async  function apiFetch() {
-    try {
-        const response = await fetch(url)
-        if (response.ok) {
-            const data = await response.json();
-            console.log(data);
-            displayResults(data);
-        } else {
-            throw Error(await response.text());
-        }
-    } catch (error) {
-        console.log(error)
-    }
-}
-
 apiFetch();
 
-function displayResults (data){
-    document.getElementById('convert-button').addEventListener('click', function() {
-        const amount = document.getElementById('amount').value;
-        const fromCurrency = document.getElementById('from-currency').value;
-        const toCurrency = document.getElementById('to-currency').value;
-
-        const rate = data.rates[toCurrency];
-        const result = amount * rate;
-
-        const formattedResult = result.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
-
-        document.getElementById('conversion-result').textContent = `${amount} ${fromCurrency} = ${formattedResult} ${toCurrency}`;
-    });
-}
 
