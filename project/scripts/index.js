aabb29537f521eafba36aa9e6b6e809695d68a52
EzeG92 <gim24001@byui.edu>
2024-10-23 01:04:35 -0300

    changes

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -1,6 +1,21 @@
 import { apiFetch } from "./api.js";
 import { createProducts, showProducts, openProductModal, setupCloseButton} from './products.js';
 
+//add event listener to menu button and nav links
+const hamButton = document.querySelector('#menuButton');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+//add content to footer
+const year = new Date().getFullYear();
+const copyrightText = `&copy; ${year} Ezk Pc Store - All rights reserved.`;
+const footerElement = document.getElementById('footer');
+footerElement.innerHTML = copyrightText;
+
 document.addEventListener('DOMContentLoaded', () => {
     // Benefits modal content
     const benefits = [
